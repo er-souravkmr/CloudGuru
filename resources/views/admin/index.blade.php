a162f04069dcea88c6b755e961ffc0f78af988ba
er-souravkmr <er.souravkmr@gmail.com>
2024-05-25 13:45:15 +0530

    "Old"

--- resources/views/admin/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('admin.layouts.main')
-
-@section('admin')
-
-      <div class="pt-5">
-        <h3>Welcome Sourav</h3>
-      </div>
-     
-@endsection
--- resources/views/admin/layouts/main.blade.php
+++
@@ -1,7 +0,0 @@
-@include('admin.layouts.header')
-@include('admin.layouts.nav')
-@include('admin.layouts.sidebar')
-
-
-@yield('admin')
-@include('admin.layouts.footer')
